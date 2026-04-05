b2f899aa288ea08184a95fb9664cd92cee8211fc
Christian Sanchez <christian.s.molina@gmail.com>
2026-04-05 13:55:14 -0600

    feat: Add metadata improvements

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,36 @@ import { geistMono, geistSans } from '@/core/constants/fonts'
 import type { Metadata } from 'next'
 import './globals.css'
 
+const title = 'Folio'
+const description = 'Folio - Tu tracker de inversiones'
+
 export const metadata: Metadata = {
-    title: 'Folio',
-    description: 'Folio - Tu tracker de inversiones'
+    title: {
+        template: `%s | ${title}`,
+        default: title
+    },
+    description,
+    metadataBase: new URL('https://project-domain.com'),
+    openGraph: {
+        title,
+        description,
+        siteName: title,
+        images: [
+            {
+                url: '/og.png',
+                width: 800,
+                height: 600,
+                alt: title
+            }
+        ],
+        type: 'website'
+    },
+    twitter: {
+        card: 'summary_large_image',
+        title,
+        description,
+        images: ['/og.png']
+    }
 }
 
 export default function RootLayout({
